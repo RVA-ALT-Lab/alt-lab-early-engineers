4be6652881effc09955410a207c0f1754871acf2
Tom Woodward <twwoodward@me.com>
2020-06-18 15:34:00 -0400

    change class of facet template

--- inc/facet.php
+++ inc/facet.php
@@ -1,6 +1,6 @@
 <?php while ( have_posts() ): the_post(); ?>
 		<div class="col-md-4 facet-lesson-info">
-			<?php echo get_the_post_thumbnail( $post->ID, 'eng-size', array('class' => 'lesson-thumb') );?>
+			<?php echo get_the_post_thumbnail( $post->ID, '', array('class' => 'lesson-thumb') );?>
 			<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 			<a class="btn btn-workshop" data-toggle="collapse" href="#<?php echo $post->post_name?>" role="button" aria-expanded="false" aria-label="more details about the <?php the_title(); ?> workshop">+</a>
 			<div class="collapse" id="<?php echo $post->post_name?>">
